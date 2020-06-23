9202c6b28345d9c6b5bdcb6e99cd3bbaa996bd14
niels.soeffers <n.soeffers@aca-it.be>
2020-06-23 22:03:27 +0200

    Initial commit

---
+++ lib/share_web.dart
@@ -0,0 +1,35 @@
+library share_web;
+
+import 'package:flutter/services.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import "dart:js";
+
+class ShareWebPlugin {
+
+    static void registerWith(Registrar registrar) {
+        final MethodChannel channel = MethodChannel(
+            'plugins.flutter.io/share',
+            const StandardMethodCodec(),
+            registrar.messenger
+        );
+        final ShareWebPlugin instance = ShareWebPlugin();
+        channel.setMethodCallHandler(instance.handleMethodCall);
+    }
+
+
+    Future<dynamic> handleMethodCall(MethodCall call) async {
+        switch (call.method) {
+            case 'share':
+                return _share(call.arguments['text']);
+            default:
+                throw PlatformException(
+                    code: 'Unimplemented',
+                    details: "The url_launcher plugin for web doesn't implement "
+                        "the method '${call.method}'");
+        }
+    }
+
+    _share(String text) async {
+        context.callMethod('open', [ 'mailto:?body=${text}', 'about:blank' ]);
+    }
+}
